8c1f565172ca1486a51f18a7966fac42c36d2fa3
Ed Sahakian <ed.sahakian@gmail.com>
2022-11-30 22:19:34 -0800

    formating

--- test-client/src/index.ts
+++ test-client/src/index.ts
@@ -1,12 +1,13 @@
-import TheOne from "@ed.sahakian/theone";
+import TheOne from '@ed.sahakian/theone'
 
 const client = new TheOne({
-  apiKey: "ZL6WU6WyLD-icp0JXGlI",
-});
+	apiKey: 'ZL6WU6WyLD-icp0JXGlI',
+})
 
+client.books
 client.books.getAll().then((res) => {
-  console.log(res);
-});
+	console.log(res)
+})
 
 // client.movies.getAll().then((res) => {
 // 	console.log(res)
