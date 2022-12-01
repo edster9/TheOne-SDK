4232656bff304d0c9bbcda70d6b71960d2ab1806
Ed Sahakian <ed.sahakian@gmail.com>
2022-11-30 23:07:07 -0800

    updated test client

--- test-client/src/index.ts
+++ test-client/src/index.ts
@@ -4,16 +4,19 @@ const client = new TheOne({
 	apiKey: 'ZL6WU6WyLD-icp0JXGlI',
 })
 
-client.books
 client.books.getAll().then((res) => {
 	console.log(res)
 })
+
+// client.books.getOne('5cf58077b53e011a64671583').then((res) => {
+// 	console.log(res)
+// })
 
 // client.movies.getAll().then((res) => {
 // 	console.log(res)
 // })
 
-// client.characters.getAll().then((res) => {
+// client.characters.getAll({ limit: '10' }).then((res) => {
 // 	console.log(res)
 // })
 
